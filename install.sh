390f31a6964c5ea501d59f36e5a6fc5b3d55b84c
RoLo <61945227+rolodoom@users.noreply.github.com>
2020-07-14 18:10:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#  _______   _______
+# |  _____| |  ___  |
+# | |       | |   | |    Rolando Ramos Torres (@rolodoom)
+# | |       | |___| |    http://rolandoramostorres.com
+# |_|       |_______|
+#  _         _______     shell-scripts installer
+# | |       |  ___  |
+# | |       | |   | |    Installer for shell-scripts
+# | |_____  | |___| |    clone repo && install scripts on /usr/local/bin
+# |_______| |_______|    Tested on Kubuntu 20.04 LTS
+#
+#
+#
 sudo mkdir shell-scripts/local-bin || return 1
 sudo cp -rf shell-scripts/scripts/* shell-scripts/local-bin || return 1
 sudo chmod -R +x shell-scripts/local-bin/*
